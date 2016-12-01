a37aae24063f0aaceb922b3bcbc998e0fe1038f8
lynn <zhanglei@lynn.com>
2016-12-01 17:49:52 +0800

    'update'

--- mobile/Public/mgotrip/test.js
+++
@@ -1,9 +0,0 @@
-/*打印JS对象并弹出*/
-function writeObj(obj){
-    var description = "";
-    for(var i in obj){
-        var property=obj[i];
-        description+=i+" = "+property+"\n";
-    }
-    alert(description);
-}
